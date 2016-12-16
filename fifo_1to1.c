faf7ab0839b27a2958836d6eabac767f33c69c49
MaximusZhou <zhougang009@gmail.com>
2016-12-16 11:15:56 +0800

    modify file encoding

--- fifo_1to1.c
+++ fifo_1to1.c
@@ -1,7 +1,7 @@
 /*
 ** 2016-12-15 19:59:20 zhougang
-** һ��lock-free��ѭ�����У������ڵ�������-��������ģʽ
-** ʵ�ַ�ʽ����liunux�ں˵�KFIFO���ݽṹ
+** 一个lock-free的循环队列，可用于单消费者-单生产者模式
+** 实现方式类似liunux内核的KFIFO数据结构
 */
 
 #include <stdlib.h>
