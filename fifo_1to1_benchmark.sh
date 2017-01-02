177339e2eb370590ed0bf2f173c9a84572afb91b
MaximusZhou <zhougang009@gmail.com>
2017-01-02 13:58:22 +0800

    modify fifo 1to1 benchmark shell test script for more accurate

--- fifo_1to1_benchmark.sh
+++ fifo_1to1_benchmark.sh
@@ -18,16 +18,27 @@ else
 fi
 
 echo "$TYPE_DESC benchmark start"
-START_SEC=`date +%s`
-START_NOS=`date +%N`
+
+SEP_SIGN="+"
+TIME_INFO=`date +%s$SEP_SIGN%N`
+
+#echo $TIME_INFO
+START_SEC=`echo $TIME_INFO | awk -F $SEP_SIGN '{print $1}'`
+START_NOS=`echo $TIME_INFO | awk -F $SEP_SIGN '{print $2}'`
+#echo "$START_SEC$SEP_SIGN$START_NOS"
 
 for((i=1;i<=$RUN_COUNT;i++))
 do 
 	./fifo_1to1_benchmark $TEST_TYPE $DATA_NUM > /dev/null
 done
 
-END_SEC=`date +%s`
-END_NOS=`date +%N`
+SEP_SIGN="+"
+TIME_INFO=`date +%s$SEP_SIGN%N`
+
+#echo $TIME_INFO
+END_SEC=`echo $TIME_INFO | awk -F $SEP_SIGN '{print $1}'`
+END_NOS=`echo $TIME_INFO | awk -F $SEP_SIGN '{print $2}'`
+#echo "$END_SEC$SEP_SIGN$END_NOS"
 
 CONSUME_MS=$((($END_SEC - $START_SEC)*1000+$END_NOS/1000000-$START_NOS/1000000))
 AVG_MS=$(($CONSUME_MS/$RUN_COUNT))
